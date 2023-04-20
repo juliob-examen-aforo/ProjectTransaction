92ae550aa786f2eb141d697a5be9d2af05ddb43b
Julio Cesar Becerra Urbina <julioc_m18@hotmail.com>
2023-04-20 09:40:28 -0500

    seq integration

--- src/app/controllers/movement.controller.js
+++ src/app/controllers/movement.controller.js
@@ -1,8 +1,11 @@
+const logProvider = require("../middlewares/logprovider");
 const movementService = require('../services/movement.service')
 
 const getTransactionsByInvoiceId = async (req, res) => {
-    const invoiceId = parseInt(req.params.invoiceId)
-    return res.send(await movementService.getTransactionsByInvoiceId(invoiceId))
+    const invoiceId = parseInt(req.params.invoiceId);
+    const transactionsByInvoice =  await movementService.getTransactionsByInvoiceId(invoiceId);
+    logProvider.info("Invoice data:", transactionsByInvoice);
+    return res.send(transactionsByInvoice)
 }
 
 module.exports = { getTransactionsByInvoiceId }
